ea13b942870c2db724a560b9cea803c63d6d188d
Mateusz Klos <novopl@gmail.com>
2017-02-04 23:45:15 +0000

    (fix) Return valid pkg fullname when inside subdir
    The initial implementation was very dumb. Now it will return
    package fullname relative to the given rootdir but won't inlcude
    parent folders if they aren't python packages.

--- src/refdoc/util.py
+++ src/refdoc/util.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Various helpers and utilities.
+"""
 from __future__ import absolute_import
-from os import walk, sep
-from os.path import relpath
+from os import walk
+from os.path import relpath, exists, join, sep as path_sep, dirname, basename
 
 import attr
 
@@ -52,14 +55,29 @@ def gen_ref_file_tree(pkgs):
     return '\n'.join(lines)
 
 
+def is_pkg(path):
+    """ Return ``True`` if the given path is a python package. """
+    return exists(join(path, '__init__.py'))
+
+
 def get_packages(rootdir):
-    """ Find all packages in the given root directory
+    """ Find all packages in the given root directory. """
+    def _get_package_full_name(rel_path):
+        curr_dir = rel_path
 
-    :param rootdir:
-    :return:
-    """
+        parents = []
+        while is_pkg(join(rootdir, curr_dir)):
+            parents.append(basename(curr_dir))
+
+            if path_sep not in curr_dir:
+                # We reached the rootdir
+                break
+
+            curr_dir = dirname(curr_dir)
+
+        return '.'.join(reversed(parents))
+
     pkgs = []
-
     for path, _, files in walk(rootdir):
         if '__init__.py' not in files:
             continue
@@ -67,7 +85,7 @@ def get_packages(rootdir):
         rel_path = relpath(path, rootdir)
 
         pkgs.append(Package(
-            fullname=rel_path.replace(sep, '.'),
+            fullname=_get_package_full_name(rel_path),
             path=path,
             relpath=rel_path,
             modules=[
