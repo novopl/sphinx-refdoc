2295626f694cee42ce9f049543358c06b9b52f10
Mateusz Klos <novopl@gmail.com>
2018-01-12 03:00:42 +0100

    Use fabops as dependency
    Easier maintenance of the management commands between the projects.
    If sphinx-refdocs needs to deviate from the mainstream fabops it can
    be easily done by copying the commands source for the last working
    version and modifying it in place.

--- fabfile.py
+++ fabfile.py
@@ -4,10 +4,33 @@ This is fabrics configuration file.
 """
 from __future__ import absolute_import
 
+
+# Configure the build
+from fabops.commands.common import conf
+conf.init({
+    'SRC_DIR': 'src',
+    'SRC_PATH': 'src/refdoc',
+    'BUILD_DIR': '.build',
+    'DOC_SRC_PATHS': 'docs',
+    'LINT_PATHS': [
+        'src/refdoc',
+    ],
+    'REFDOC_PATHS': [
+        'src/refdoc',
+        'env/lib/python3.6/site-packages/fabops/commands'
+    ],
+    'TEST_TYPES': {
+        'default': {'paths': ['test']}
+    }
+})
+
+
 # Import all commands
-from ops.commands.clean import *
-from ops.commands.docs import *
-from ops.commands.git import *
-from ops.commands.lint import *
-from ops.commands.release import *
-from ops.commands.test import *
+from fabops.commands.clean import *
+from fabops.commands.docs import *
+from fabops.commands.release import *
+from fabops.commands.frontend import *
+from fabops.commands.git import *
+from fabops.commands.lint import *
+from fabops.commands.ops import *
+from fabops.commands.test import *
