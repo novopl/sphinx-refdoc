a1915a63246f3817b11c70631d0a24f89aab626b
Mateusz Klos <novopl@gmail.com>
2018-10-07 00:44:30 +0100

    Releasing v0.3

--- src/refdoc/__init__.py
+++ src/refdoc/__init__.py
@@ -5,7 +5,7 @@ This module implements python reference documentation generator for sphinx.
 from __future__ import absolute_import
 from .logic import generate_docs
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 __all__ = [
     'generate_docs'
